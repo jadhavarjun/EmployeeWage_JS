cd4c11ba4ba070e961c6910206990099c9ebaf2d
arjunjava <jadhavarjun53@gmail.com>
2021-02-03 01:44:29 +0530

    Refactor: existing function and get employee work hour

--- FunctionalProgram/utility.js
+++ FunctionalProgram/utility.js
@@ -1,11 +1,18 @@
 class Utility {
 
-    EMP_RATE_PER_HOUR = 20;
+    EMP_RATE_PER_HOUR = 20
+    NUM_OF_WORKING_DAYS = 21
+    MAX_HRS_IN_MONTH = 100;
     IS_FULL_TIME = 1;
     IS_PART_TIME = 2;
     EMP_HR = 0;
+    DAY = 0;
     
-    empCheck
+    empCheck = 0
+    empWage = 0
+    totalWage = 0
+    totalEmpHrs = 0 
+    totalWorkingDays = 0
 
     empAttendance() {
 
@@ -13,26 +20,41 @@ class Utility {
         return this.empCheck;
     }
 
-    mainEmployeeWage = () => {
-        
-        this.empAttendance();
+    empDailyWage = () => {
+        this.empWage = this.EMP_HR * this.EMP_RATE_PER_HOUR;
+        return this.empWage;
+    }
+
+    getWorkingHrs = () =>{
 
         switch(this.empCheck) {
-            
-            case this.IS_FULL_TIME:
+                
+            case this.IS_PART_TIME:
                     this.EMP_HR = 8
                     break;
             
-            case this.IS_PART_TIME:
+            case this.IS_FULL_TIME:
                     this.EMP_HR = 4    
                     break;
                 
             default: 
-	            console.log("Employee Is Absent..!!");
+                this.EMP_HR = 0
         }
-        //this.empDailyWage();
-        let empWage = this.EMP_HR * this.EMP_RATE_PER_HOUR;
-        console.log("\n * The Daily Eamployee Wage Is : "+empWage);
+        return this.EMP_HR
+    }
+
+    mainEmployeeWage = () => {
+
+        this.empAttendance();
+
+        while (this.totalEmpHrs <= this.MAX_HRS_IN_MONTH && this.totalWorkingDays < this.NUM_OF_WORKING_DAYS) {
+            this.totalWorkingDays++;
+
+            this.getWorkingHrs(this.empCheck);
+            this.totalEmpHrs += this.EMP_HR;
+        }
+        this.totalEmpWage = this.totalEmpHrs * this.EMP_RATE_PER_HOUR;
+        console.log("Working Hr : "+this.totalEmpHrs+", Total Wage Is : "+this.totalEmpWage);  
     }
 }
 
