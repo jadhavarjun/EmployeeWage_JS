e5466f20c7bb082fffe3b5ed335ad0487f02586a
arjunjava <jadhavarjun53@gmail.com>
2021-02-03 18:05:00 +0530

    Add:  Store Day Daily and Total Wage using Map

--- FunctionalProgram/EmployeeWage.js
+++ FunctionalProgram/EmployeeWage.js
@@ -1,18 +1,18 @@
 class Utility {
 
     EMP_RATE_PER_HOUR = 20
-    NUM_OF_WORKING_DAYS = 20
-    MAX_HRS_IN_MONTH = 160;
+    NUM_OF_WORKING_DAYS = 21
+    MAX_HRS_IN_MONTH = 100;
     IS_FULL_TIME = 1;
     IS_PART_TIME = 2;
     EMP_HR = 0;
     DAY = 0;
     
-    empCheck = 0
-    empWage = 0
-    totalWage = 0
-    totalEmpHrs = 0 
-    totalWorkingDays = 0
+    empCheck = 0;
+    empWage = 0;
+    totalWage = 0;
+    totalEmpHrs = 0; 
+    totalWorkingDays = 0;
 
     empAttendance() {
 
@@ -30,24 +30,22 @@ class Utility {
         switch(this.empCheck) {
                 
             case this.IS_PART_TIME:
-                    this.EMP_HR = 8
+                    this.EMP_HR = 8;
                     break;
             
             case this.IS_FULL_TIME:
-                    this.EMP_HR = 4    
+                    this.EMP_HR = 4;   
                     break;
                 
             default: 
-                this.EMP_HR = 0
+                this.EMP_HR = 0;
         }
         return this.EMP_HR
     }
 
     mainEmployeeWage = () => {
 
-        let empDailyWage = new Array();
-
-        
+        let empDailyWage = new Map();
 
         while (this.totalEmpHrs <= this.MAX_HRS_IN_MONTH && this.totalWorkingDays < this.NUM_OF_WORKING_DAYS) {
             this.totalWorkingDays++;
@@ -55,11 +53,11 @@ class Utility {
             this.empAttendance();
             this.EMP_HR = this.getWorkingHrs(this.empCheck);
             this.totalEmpHrs += this.EMP_HR;
-            empDailyWage.push(this.empDailyWage(this.EMP_HR))
+            empDailyWage.set("Day"+this.totalWorkingDays, this.empDailyWage(this.EMP_HR))
         }
         this.totalEmpWage = this.totalEmpHrs * this.EMP_RATE_PER_HOUR;
-        console.log("\n * The Daily Wage Of Employee Is : "+empDailyWage);
-        console.log("\n * The Monthly Wage Of Employee Is : "+this.totalEmpWage);
+        console.log(`The Monthly Wage Of Employee Is : ${this.totalEmpWage} \n`);
+        console.log(empDailyWage);
     }
 }
 
